a0356503d581ae1c88b42535d53b1e991785b67b
Marcelo Cueto <cueto@live.cl>
2020-01-20 19:10:49 -0300

    Add navbar and related links to App component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <template>
   <v-app>
+
+    <v-navigation-drawer
+      v-model="drawerRight"
+      app
+      clipped
+      right
+    >
+      <v-list dense>
+        <div class="v-subheader theme--light">
+          Take a look to Auth0 + Django REST Framework
+        </div>
+
+        <!-- <v-list-item @click.stop="right = !right">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item> -->
+
+        <v-list-item @click.stop="right = !right">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              <a href="https://github.com/mcueto/djangorestframework-auth0">djangorestframework-auth0</a>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item @click.stop="right = !right">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              <a href="https://github.com/mcueto/djangorestframework-auth0_sample">djangorestframework-auth0_sample</a>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
       color="primary"
@@ -28,33 +74,46 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/mcueto/auth0-vue-frontend_sample"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Clone this project</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight" />
+
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+  },
+
+  props: {
+    source: String,
   },
 
   data: () => ({
-    //
+    drawer: null,
+    drawerRight: null,
+    right: false,
+    left: false,
   }),
+
 };
 </script>
